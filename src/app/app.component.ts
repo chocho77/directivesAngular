dbe07f8d58cc4f8673542a899ea5560d2173737e
chavdar77 <chavmom@gmail.com>
2023-06-15 15:50:31 -0400

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,19 @@ export class AppComponent {
   title = 'directivesAngular';
   postArray:Array<string> = ['Post 1', 'Post 2', 'Post 3', 'Post 4', 'Post 5'];
 
+  objArray:Array<object> = [
+    {id:1, postTitle: 'Post 1'},
+    {id:2, postTitle: 'Post 2'},
+    {id:3, postTitle: 'Post 3'},
+    {id:4, postTitle: 'Post 4'},
+    {id:5, postTitle: 'Post 5'},
+  ]
+
+  constructor(){
+    for(let i = 0; i < this.postArray.length; i++){
+      console.log(this.postArray[i]);
+      
+    }
+  }
+
 }
